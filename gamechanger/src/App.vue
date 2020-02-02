57c1dd6497846633d281aa116bf29bec7a067409
pchan005 <chanpj@uci.edu>
2020-02-02 13:24:14 -0800

    Added NavBar component

--- gamechanger/src/App.vue
+++ gamechanger/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
@@ -21,8 +20,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin:0;
 }
 </style>
---
+++ gamechanger/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav id="navbar">
+        <a href="/">Test</a>
+        <a href="/">Test2</a>
+        <a href="/">Test3</a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+
+    }
+</script>
+
+<style scoped>
+#navbar {
+    background-color:#2C2C2C;
+    padding:15px;
+    text-align: left;
+}
+
+nav a {
+    padding: 0px 20px 20px 0px;
+    color: white;
+}
+</style>
