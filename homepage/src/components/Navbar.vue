d6c71cb7d4573bff14f34ade92549c7400c7b188
pchan005 <chanpj@uci.edu>
2020-04-06 16:33:11 -0700

    Update Navbar.vue

--- homepage/src/components/Navbar.vue
+++ homepage/src/components/Navbar.vue
@@ -1,22 +1,38 @@
 <template>
     <nav>
-        <v-app-bar flat class="grey darken-4" v-if="loggedIn">
-            <img :src="require('../assets/logo.png')" height="100%"/>
+        <v-app-bar v-if="loggedIn" flat app class="grey darken-4">
+          <img :src="require('../assets/logo.png')" height="100%"/>
             <v-toolbar-title class="ml-12 white--text subtitle-1">
                 <router-link to="/homepage"><span class="ml-1 mr-7 white--text">Home</span></router-link>
                 <router-link to="/showcase"><span class="ml-7 mr-7 white--text">Showcase</span></router-link>
                 <router-link to="/events"><span class="ml-7 mr-7 white--text">Events</span></router-link>
             </v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn outlined color="#F6F6F6" width="90" class="mr-7 body-2">Upload</v-btn>
-            <v-avatar class="mr-7">
-                <img
-                    src="https://cdn.vuetifyjs.com/images/john.jpg"
-                    alt="John"
-                >
-                </v-avatar>
+            <router-link to="/projectsubmissionpage"><v-btn outlined color="#F6F6F6" class="ma-2 mr-7 body-2"><v-icon left>fas fa-upload</v-icon>Upload</v-btn></router-link>
+            <router-link to="#">
+                <v-menu open-on-hover top offset-y>
+                    <template v-slot:activator="{ on }">
+                        <v-btn
+                        color="#F6F6F6"
+                        v-on="on"
+                        >
+                        <v-icon>fas fa-user</v-icon>
+                        </v-btn>
+                    </template>
+
+                    <v-list>
+                        <!-- add @click="" to add a function for each button -->
+                        <v-list-item
+                        v-for="(item, index) in dropdown_items"
+                        :key="index"
+                        >
+                        <v-list-item-title>{{ item }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </router-link>
         </v-app-bar>
-        <v-app-bar v-else flat class="grey darken-4">
+        <v-app-bar v-else flat app class="grey darken-4">
           <img :src="require('../assets/logo.png')" height="100%"/>
             <v-toolbar-title class="ml-12 white--text subtitle-1">
                 <router-link to="/homepage"><span class="ml-1 mr-7 white--text">Home</span></router-link>
@@ -34,7 +50,8 @@
 export default {
     data() {
         return {
-            loggedIn: false
+            loggedIn: false,
+            dropdown_items: ['Profile', 'Logout']
         }
     }
     
