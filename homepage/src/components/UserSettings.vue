2d1f300daa41f6afe402094ea4b6ca7508804ada
pchan005 <chanpj@uci.edu>
2020-05-18 10:00:36 -0700

    Update UserSettings.vue

--- homepage/src/components/UserSettings.vue
+++ homepage/src/components/UserSettings.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
     <v-app class="background-color">
         <Navbar2 />
         <!-- Form page -->
@@ -18,20 +19,71 @@
                 </v-flex>
                 <v-flex md2></v-flex>
             </v-layout>
+=======
+    <v-app class="grey lighten-4">
+        <Navbar />
+        <!-- Form page -->
+        <v-container class="header-spacing">
+            <v-row justify="center">
+                <v-col cols="12" md="8">
+                    <v-card outlined class="pa-3">
+                        <v-card-title class="font-weight-bold headline">Settings</v-card-title>
+                            <v-row align="center">
+                                <v-col class="pr-0" cols="12" md="3">
+                                    <v-card-text class="font-weight-medium">Display Name</v-card-text>
+                                </v-col>
+                                <v-col class="pl-0 ml-n6" cols="12" md="5">
+                                    <v-text-field dense outlined :value="userSubmission.userName"></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row align="center">
+                                <v-col class="pr-0" cols="12" md="3">
+                                    <v-card-text class="font-weight-medium">Password</v-card-text>
+                                </v-col>
+                                <v-col class="pl-0 ml-n6" cols="12" md="5">
+                                    <v-text-field dense outlined :value="userSubmission.userPassword"></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col class="pr-0" cols="12" md="3">
+                                    <v-card-text class="font-weight-medium">Email</v-card-text>
+                                </v-col>
+                                <v-col class="pl-0 ml-n6" cols="12" md="5">
+                                    <v-text-field dense outlined :value="userSubmission.userEmail"></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-card-actions>
+                                <v-btn dark depressed large color="#4DB848" class="body-1 font-weight-medium">Save</v-btn>
+                            </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
+>>>>>>> Stashed changes
         </v-container>
+        <Footer />
     </v-app>
 </template>
 
 <script>
+<<<<<<< Updated upstream
 import Navbar2 from "./Navbar-2";
 
 export default {
     components: {
         Navbar2
+=======
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default {
+    components: {
+        Navbar,
+        Footer
+>>>>>>> Stashed changes
     },
     // This is for the user information. Data right now is hard coded.
     data: () => ({
-        userSubmission:
+        userSubmission: 
         { 
             userName: 'Sam',
             userPassword: '********',
@@ -43,40 +95,11 @@ export default {
 </script>
 
 <style scoped>
-  .background-color {
-    background: #E5E5E5;
-    padding-bottom: 4em;
-  }
-
-  .form-color {
-    background: #FFFFFF;
-    padding-top: 2em;
-    padding-bottom: 6em;
-    padding-left: 6em;
-    padding-right: 6em;
-  }
-
-  .left-tab {
-      padding-left: 3em;
-  }
-
-  .right-tab {
-      padding-right: 1em;
-  }
-
   .gamechanger-green {
       color: #4DB848;
   }
 
   .header-spacing {
-    margin-top: 9em;
-  }
-
-  .header-padding {
-      margin-bottom: 1.5em;
-  }
-
-  .bigger-font {
-      font-size: 18px;
+    margin-top: 5em;
   }
 </style>
