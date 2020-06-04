b75b7f9d541db52bac316d803e5158271f37f47f
rardosa <34051984+rardosa@users.noreply.github.com>
2020-06-04 13:14:11 -0700

    responsiveness pt.3
    Made changes to confirm sign up and forgot password

--- homepage/src/components/ConfirmSignUp.vue
+++ homepage/src/components/ConfirmSignUp.vue
@@ -2,13 +2,22 @@
     <v-app class="grey lighten-4">
         <Navbar />
         <v-layout row>
-                <v-flex md6>
-                    <v-img height="vh" src="../assets/keyboard.png" alt="Picture of hand typing on keyboard">
+            <v-flex md6 sm12>
+                <!--100% picture are larger screen size-->
+                <v-container d-none d-md-block>
+                    <v-img class="no-margin" height="100vh" src="../assets/keyboard.png" alt="Picture of hand typing on keyboard">
                         <h1 class="tagline">Share the story of<br> your creation</h1>
                     </v-img>
-                </v-flex>
+                </v-container>
+                <!--40% picture are larger screen size-->
+                <v-container d-flex d-md-none>
+                    <v-img height="40vh" src="../assets/keyboard.png" alt="Picture of hand typing on keyboard">
+                        <h1 class="tagline">Share the story of<br> your creation</h1>
+                    </v-img>
+                </v-container>
+            </v-flex>
             <v-layout column md7 text-center>
-                <v-flex xs2></v-flex>
+                <v-flex xs2 d-none d-md-block></v-flex>
                 <v-flex xs1 class="mb-10">
                     <h1>Confirm Signing Up</h1>
                 </v-flex>
@@ -24,6 +33,10 @@
                         disabled
                     ></v-text-field>
                 </v-flex>
+                 <!--Add spacing when small-->
+                <v-container d-md-none>
+                    <v-spacer></v-spacer>
+                </v-container>
                 <!--This is the input box for the user's confirmation code-->
                 <v-flex xs1>
                     <v-text-field 
@@ -36,11 +49,20 @@
                         solo
                     ></v-text-field>
                 </v-flex>
+                 <!--Add spacing when small-->
+                <v-container d-md-none>
+                    <v-spacer></v-spacer>
+                </v-container>
                 <p><span class="underlined"><a href="#">Resend code</a></span></p>
                 <v-flex xs1></v-flex>
                 <v-flex xs2>
                     <!--This is the submit button that you have to bind an on-click event for-->
-                    <v-btn height="4em" width="13em" color="#4DB848" class="white--text body-1" @click="confirmSignUp">Confirm</v-btn>
+                    <v-flex  d-none d-md-block>
+                        <v-btn height="4em" width="13em" color="#4DB848" class="white--text body-1" @click="confirmSignUp">Confirm</v-btn>
+                    </v-flex>
+                     <v-container d-flex d-md-none>
+                        <v-btn height="3em" width="9em" color="#4DB848" class="center-block white--text body-1" @click='confirmSignUp'>Confirm</v-btn>
+                    </v-container>
                     <!--Eventually, add a link to take the user to the login page when they click the span-->
                     <p class="mt-6">Already have an account? <router-link to="/login"><span class="underlined">Log In!</span></router-link></p>
                 </v-flex>
@@ -100,5 +122,10 @@ export default {
     margin: 2em;
     font-size:2.5em;
 }
+
+.no-margin {
+    margin-bottom: -2em;
+}
+
 
 </style>
