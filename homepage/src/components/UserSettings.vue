2e7fcebba0494877d531b3a507ba62a290cbbcfd
Michelle Norman <michellenorman239@gmail.com>
2020-06-08 14:40:53 -0700

    update user settings page
    - added twitter section,
    - slightly changed UI
    - can only take bio, linkedin, github, twitter, and username changes.

--- homepage/src/components/UserSettings.vue
+++ homepage/src/components/UserSettings.vue
@@ -9,10 +9,10 @@
                     <v-card-title class="font-weight-bold headline">Settings</v-card-title>
                     <v-row align="center">
                         <v-col class="pr-0" cols="12" md="3">
-                            <v-card-text class="font-weight-medium">Display Name</v-card-text>
+                            <v-card-text class="font-weight-medium">Email</v-card-text>
                         </v-col>
                         <v-col class="pl-0 ml-n6" cols="12" md="5">
-                            <v-text-field color="#4DB848" readonly dense outlined :prefix="userSubmission.userName"></v-text-field>
+                            <v-label color="#4DB848">{{ userSubmission.userEmail }}</v-label>
                         </v-col>
                     </v-row>
                     <v-row align="center">
@@ -20,19 +20,17 @@
                             <v-card-text class="font-weight-medium">Password</v-card-text>
                         </v-col>
                         <v-col class="pl-0 ml-n6" cols="12" md="5">
-                            <!-- <v-text-field v-if="changingNames" color="#4DB848" dense outlined :value="userSubmission.userPassword" :type="show1 ? 'text' : 'password'"></v-text-field>
-                            <v-text-field v-else color="#4DB848" readonly dense outlined :prefix="userSubmission.userPassword" :type="show1 ? 'text' : 'password'"></v-text-field> -->
-                            <v-text-field v-if="changingNames" color="#4DB848" dense outlined :value="userSubmission.userPassword"></v-text-field>
-                            <v-text-field v-else color="#4DB848" readonly dense outlined :prefix="userSubmission.userPassword"></v-text-field>
+                            <!-- add input field for password here when implemented -->
+                            <v-label color="#4DB848" type="password">{{hidePassword(userSubmission.userPassword)}}</v-label>
                         </v-col>
                     </v-row>
                     <v-row align="center">
                         <v-col class="pr-0" cols="12" md="3">
-                            <v-card-text class="font-weight-medium">Email</v-card-text>
+                            <v-card-text class="font-weight-medium">Username</v-card-text>
                         </v-col>
                         <v-col class="pl-0 ml-n6" cols="12" md="5">
-                            <v-text-field v-if="changingNames" color="#4DB848" dense outlined :value="userSubmission.userEmail"></v-text-field>
-                            <v-text-field v-else color="#4DB848" readonly dense outlined :prefix="userSubmission.userEmail"></v-text-field>
+                            <v-text-field v-if="changingNames" v-model="userSubmission.userName" color="#4DB848" dense outlined :value="userSubmission.userEmail"></v-text-field>
+                            <v-label v-else color="#4DB848">{{ userSubmission.userName }}</v-label>
                         </v-col>
                     </v-row>
                     <v-row align="center">
@@ -40,17 +38,17 @@
                             <v-card-text class="font-weight-medium">Profile Bio</v-card-text>
                         </v-col>
                         <v-col class="pl-0 ml-n6" cols="12" md="7">
-                            <v-textarea v-if="changingNames" :rows="4" :row-height="32" color="#4DB848" dense outlined :value="userSubmission.userBio"></v-textarea>
-                            <v-textarea v-else :rows="4" :row-height="32" color="#4DB848" readonly dense outlined :prefix="userSubmission.userBio"></v-textarea>
+                            <v-textarea v-if="changingNames" v-model="userSubmission.userBio" :rows="4" :row-height="32" color="#4DB848" dense outlined :value="userSubmission.userBio"></v-textarea>
+                            <v-label v-else :rows="4" :row-height="32" color="#4DB848">{{ userSubmission.userBio }}</v-label>
                         </v-col>
                     </v-row>
                     <v-row align="center">
                         <v-col class="pr-0" cols="12" md="3">
-                            <v-card-text class="font-weight-medium">linkedIn</v-card-text>
+                            <v-card-text class="font-weight-medium">LinkedIn</v-card-text>
                         </v-col>
                         <v-col class="pl-0 ml-n6" cols="12" md="7">
-                            <v-text-field v-if="changingNames" autofocus color="#4DB848" dense outlined :value="userSubmission.linkedIn"></v-text-field>
-                            <v-text-field v-else color="#4DB848" autofocus readonly dense outlined :value="userSubmission.linkedIn"></v-text-field>
+                            <v-text-field v-if="changingNames" v-model="userSubmission.linkedIn" autofocus color="#4DB848" dense outlined :value="userSubmission.linkedIn"></v-text-field>
+                            <v-label v-else color="#4DB848">{{ userSubmission.linkedIn}}</v-label>
                         </v-col>
                     </v-row>
                     <v-row align="center">
@@ -58,12 +56,21 @@
                             <v-card-text class="font-weight-medium">Github</v-card-text>
                         </v-col>
                         <v-col class="pl-0 ml-n6" cols="12" md="7">
-                            <v-text-field v-if="changingNames" color="#4DB848" dense outlined :value="userSubmission.github"></v-text-field>
-                            <v-text-field v-else color="#4DB848" readonly dense outlined :prefix="userSubmission.github"></v-text-field>
+                            <v-text-field v-if="changingNames" v-model="userSubmission.github" color="#4DB848" dense outlined :value="userSubmission.github"></v-text-field>
+                            <v-label v-else color="#4DB848">{{ userSubmission.github }}</v-label>
+                        </v-col>
+                    </v-row>
+                    <v-row align="center">
+                        <v-col class="pr-0" cols="12" md="3">
+                            <v-card-text class="font-weight-medium">Twitter</v-card-text>
+                        </v-col>
+                        <v-col class="pl-0 ml-n6" cols="12" md="7">
+                            <v-text-field v-if="changingNames" v-model="userSubmission.twitter" autofocus color="#4DB848" dense outlined :value="userSubmission.twitter"></v-text-field>
+                            <v-label v-else color="#4DB848">{{ userSubmission.twitter }}</v-label>
                         </v-col>
                     </v-row>
                     <v-card-actions v-if="changingNames"> 
-                        <v-btn @click="success = true" dark depressed large color="#4DB848" class="ma-2 body-1 font-weight-medium">
+                        <v-btn v-on:click="saveChanges()" @click="success = true" dark depressed large color="#4DB848" class="ma-2 body-1 font-weight-medium">
                             <v-icon left>far fa-save</v-icon>Save
                         </v-btn>
                         <v-snackbar top color="success" v-model="success" timeout="2000">
@@ -75,7 +82,7 @@
                         </v-btn>
                     </v-card-actions>
                     <v-card-actions v-else>
-                        <v-btn @click="updateInformation" dark depressed large color="#4DB848" class="ma-2 body-1 font-weight-medium">
+                        <v-btn v-on:click="updateInformation()" dark depressed large color="#4DB848" class="ma-2 body-1 font-weight-medium">
                             <v-icon left>far fa-edit</v-icon>Edit
                         </v-btn>
                     </v-card-actions>
@@ -110,12 +117,13 @@ export default {
             // facebook: 'link'
             userName: '',
             userPassword: '',
+            userHiddenPassword: '',
             userEmail: '',
             userPicture: '',
             userBio: '',
             linkedIn: '',
             github: '', 
-            facebook: ''
+            twitter: ''
         },
         changingNames: false,
         success: false,
@@ -124,35 +132,62 @@ export default {
 
     methods: {
         updateInformation() {
-            this.changingNames = false;
-        },
-        cancelChanges() {
             this.changingNames = true;
+            window.scrollTo(0,0)
         },
-        saveChanges(dynamodb) {
+
+        cancelChanges() {
+            this.changingNames = false;
+            window.scrollTo(0,0);
+        },
+
+        saveChanges() {
+            var AWS = require('aws-sdk');
+            // Initialize the Amazon Cognito credentials provider
+            AWS.config.region = 'us-west-2'; // Region
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId: 'us-west-2:c8838837-ac29-45f7-b5c2-6ec245a55ed1',
+            });
+            var dynamodb = new AWS.DynamoDB.DocumentClient();
+
             var params = {
                 TableName: "user-info",
-                Item: {
-                    "email": this.userSubmission.userEmail,
-                    "password": this.userSubmission.userPassword,
-                    "displayName": this.userSubmission.userName,
+                Key: {
+                    email: this.userSubmission.userEmail
+                },
+                UpdateExpression: "SET displayName=:name, bio=:bio, socialMedia.Github=:github, socialMedia.Twitter=:twitter, socialMedia.LinkedIn=:linkedin",
+                ExpressionAttributeValues: {
+                    ":name": this.userSubmission.userName,
+                    ":bio": this.userSubmission.userBio,
+                    ":twitter": this.userSubmission.twitter,
+                    ":github": this.userSubmission.github,
+                    ":linkedin": this.userSubmission.linkedIn
                 }
             }
 
-            dynamodb.put(params, function(err, data) {
+            dynamodb.update(params).promise().then().catch(err => {
                 if (err) {
-                    console.log(err);
+                    console.log(err)
                 }
                 else {
-                    console.log(data);
-                    location.reload();
+                    console.log('success')
                 }
-            });
+            })
+
+            this.changingNames = false;
+            window.scrollTo(0,0)
+        },
+
+        hidePassword(password) {
+            return ('*'.repeat(password.length))
         }
+
     },
     async created() {
         //scroll to the top 
         window.scrollTo(0, 0)
+
+        this.changingNames = false
 
         var AWS = require('aws-sdk');
         // Initialize the Amazon Cognito credentials provider
@@ -166,25 +201,26 @@ export default {
         var userEmail = await Auth.currentUserInfo().then(user => {
             return user.attributes
         })
-        
-        //How to tell which user has logged in? Right now, hard coded
-        var params = {
-            TableName: "user-info",
-            Key: {
-                "email": userEmail,
-            },
+
+        var user_params = {
+          TableName: 'user-info',
+          Key: {
+            "email": userEmail.email
+          }
         }
-            
-        dynamodb.get(params).promise().then(function(data) 
-        {
-        //    this.userSubmission.userName = data.Item.displayName;
-        //    this.userSubmission.userPassword = data.Item.password;
-        //    this.userSubmission.userEmail = data.Item.email;
-        console.log(data.Item)
-        }).catch(function(err)
-        {
-            console.log(err);
-        });
+        
+        dynamodb.get(user_params).promise().then(user => {
+            this.userSubmission.userName = user.Item.displayName
+            this.userSubmission.userBio = user.Item.bio
+            this.userSubmission.userEmail = user.Item.email
+            this.userSubmission.userPassword = user.Item.password
+            this.userSubmission.github = user.Item.socialMedia.Github
+            this.userSubmission.twitter = user.Item.socialMedia.Twitter
+            this.userSubmission.linkedIn = user.Item.socialMedia.LinkedIn
+
+        }).catch(err => {
+            console.log(err)
+        })
     },
 }
 </script>
